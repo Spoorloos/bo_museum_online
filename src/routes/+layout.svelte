6ffee7328c9caeff65927acb67f8b813442eb1e3
Mick Negenman <36791@ma-web.nl>
2023-11-24 10:26:40 +0100

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<!-- Script -->
+<script>
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+</script>
+
+<!-- Page -->
+<Header/>
+<slot/>
+<Footer/>
+
+<!-- Style -->
+<style>
+    :global(body) {
+        color: white;
+        background-color: black;
+        font-family: sans-serif;
+    }
+</style>
