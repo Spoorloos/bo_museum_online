0eb9157fdcd52d9bf6741d49fc922dd69bfeabbb
Mick Negenman <36791@ma-web.nl>
2023-11-28 12:45:44 +0100

    Added language selector

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,19 +1,31 @@
-<script>
+<script lang="ts">
     import "$lib/styles/Buttons.css";
+    import language from "$lib/stores/language.ts";
+
+    let currentLanguage = "en";
+
+    $: (async() => {
+        language.set(await import(`$lib/lang/${currentLanguage}.json`));
+    })();
 </script>
 
 <header>
     <h1>Museum Online</h1>
     <div id="links">
-        <button class="fill-button">What's on</button>
-        <button class="fill-button">About us</button>
-        <button class="fill-button">Contact</button>
-        <button class="fill-button">Blog</button>
+        <select title="select-lang" id="language-picker-select" bind:value={currentLanguage}>
+            <option lang="en" value="en" selected>EN</option>
+            <option lang="nl" value="nl">NL</option>
+        </select>
+
+        <button class="fill-button">{$language?.header?.whatsOn}</button>
+        <button class="fill-button">{$language?.header?.aboutUs}</button>
+        <button class="fill-button">{$language?.header?.contact}</button>
+        <button class="fill-button">{$language?.header?.blog}</button>
     </div>
 </header>
 
 <style>
-    h1, h4 {
+    h1 {
         margin: 0;
     }
 
@@ -44,5 +56,20 @@
         display: inline-flex;
         justify-content: space-between;
         gap: 20px;
+    }
+
+    #language-picker-select {
+        color: white;
+        background-color: transparent;
+        border: none;
+        border-bottom: thin solid white;
+        font-family: sans-serif;
+        font-weight: bold;
+        outline: none;
+        cursor: pointer;
+
+        & option {
+            background-color: black;
+        }
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <!-- Script -->
-<script>
+<script lang="ts">
     import "$lib/styles/global.css";
 
     import Header from "$lib/components/Header.svelte";
