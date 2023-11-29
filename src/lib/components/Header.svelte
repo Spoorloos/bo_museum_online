216fdcd23edb1ab601ddbf766869e3483cd40d36
Mick Negenman <36791@ma-web.nl>
2023-11-29 12:17:49 +0100

    Added paragraph with animations

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "$lib/styles/buttons.css";
     import language from "$lib/stores/language.ts";
+	import { slide } from "svelte/transition";
 
     let currentLanguage = "en";
 
@@ -10,20 +11,16 @@
 </script>
 
 <header>
-    <img
-        id="logo"
-        alt="logo"
-        src="logo_and_title.png"/>
-    <nav>
+    <img id="logo" alt="logo" src="logo_and_title.png" class="fade-slide"/>
+    <nav class="fade-slide">
         <select title="select-lang" id="language-picker-select" bind:value={currentLanguage}>
             <option lang="en" value="en">EN</option>
             <option lang="nl" value="nl">NL</option>
         </select>
 
-        <button class="fill-button" type="button">{$language.header.whatsOn}</button>
-        <button class="fill-button" type="button">{$language.header.aboutUs}</button>
-        <button class="fill-button" type="button">{$language.header.contact}</button>
-        <button class="fill-button" type="button">{$language.header.blog}</button>
+        {#each Object.values($language.header) as head}
+            <button class="fill-button" type="button">{head}</button>
+        {/each}
     </nav>
 </header>
 
@@ -39,27 +36,10 @@
         padding: 15px;
         height: 50px;
         box-shadow: 0 0 200px 100px black;
-        z-index: 1;
     }
 
     #logo {
         height: 100%
-    }
-
-    header > * {
-        position: relative;
-        animation: slide-in 1s both;
-    }
-
-    @keyframes slide-in {
-        from {
-            opacity: 0;
-            bottom: 50px;
-        }
-        to {
-            opacity: 1;
-            bottom: 0;
-        }
     }
 
     nav {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,49 @@
+<!-- Script -->
+<script>
+</script>
+
+<!-- Content -->
 <svelte:head>
-    <title>Museum </title>
+    <title>Museum Online</title>
 </svelte:head>
 
-<main>
-</main>
+<div id="content">
+    <h1>NXT Museum</h1>
+    <p>
+        Hello World!
+    </p>
+</div>
 
+<!-- Style -->
 <style>
-    main {
-        position: absolute;
-        width: 100vw;
-        height: calc(100% - 50px);
-        background-image: url("background.png");
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-        background-position: center;
+    #content {
+        display: inline-block;
+        position: relative;
+        top: 100px;
+        left: 100px;
+        padding: 25px;
+        animation: fade-in 1s both;
+        border-left: 1px solid white;
+    }
+
+    @keyframes fade-in {
+        from {
+            opacity: 0;
+            left: 50px;
+        }
+        to {
+            opacity: 1;
+            left: 100px;
+        }
+    }
+
+    #content h1 {
+        font-size: 100px;
+        margin: 0;  
+    }
+
+    #content p {
+        font-family: 'Segoe UI';
+        font-weight: 200;
     }
 </style>
