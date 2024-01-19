8cf745ee4916328da386755e0240a77a14160475
naaaaaaaaaaaaaaaaaaaaaaaam <jannesveuger@xs4all.nl>
2024-01-19 09:41:09 +0100

    added some images

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,40 @@
-<footer>Copyright © 2023</footer>
+<!-- <footer>Copyright © 2023</footer> -->
+<script>
+    let canvas = document.getElementsByClassName("social_media")
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/img/instagram.svg", true);
+
+</script>
+
+<footer>
+    <div id="left"><canvas class="social_media"></canvas></div>
+    <div id="right">
+        <img src="/img/routebeschrijving_nxt.png" alt="" />
+    </div>
+</footer>
 
 <style>
+    #left,
+    #right {
+        width: 50vw;
+        height: 500px;
+        float: left;
+    }
+    /* #right {
+        width:;
+    }
+    #left {
+    } */
+
     footer {
         display: flex;
-        justify-content: center;
         font-family: sans-serif;
+    }
+    .social_media{
+        width: 100%;
+        height: 100%;
+    }
+    #right img {
+        float: left;
     }
 </style>
--- src/lib/components/ReviewCard.svelte
+++ src/lib/components/ReviewCard.svelte
@@ -1,6 +1,17 @@
+<script>
+    let name = "mick"
+    let content = "KUD" 
+    let score = 10
+    
+    function scoreToText(){
+
+    }
+</script>
+
 <div class="review-card">
-    <h1>My Review</h1>
+    <h1>{content}</h1><box-icon type='solid' name='star-half'></box-icon>
 </div>
+
 
 <style>
     h1 {
@@ -10,6 +21,7 @@
     .review-card {
         position: relative;
         height: 100%;
+        width: 40vw;
         padding: 20px;
         box-sizing: border-box;
         border: 1px solid white;
