adbb7f7d53bfcf08c4e3c188935973c9fa07bee1
codamStudent <37171@ma-web.nl>
2024-01-28 11:46:41 +0100

    only changed whats_on, pls warn me if other hsit broke

---
+++ src/lib/components/CardSelection.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { onMount } from "svelte";
+
+    let headers = [];
+    let selectedCard = 0;
+
+    function selectCard(index) {
+        const cards = document.querySelectorAll(".content > div");
+
+        // Hide all non-selected cards
+        cards.forEach((card, i) => {
+            if (i === index) { // ew 
+                setTimeout(() => {card.style.display = "block"}, 200)
+                setTimeout(() => {card.style.opacity = "1"}, 300);
+            } else {
+                card.style.opacity = "0";
+                setTimeout(() => {card.style.display = "none"}, 300);
+            }
+        });
+
+        selectedCard = index;
+    }
+
+    onMount(() => {
+        // Create headers
+        const cards = document.querySelectorAll(".content > div");
+        
+        headers = Array.from(cards).map((card, index) => ({
+            img: card.querySelector(".card-img").src,
+            title: card.querySelector(".card-title").innerText,
+            // content: card.querySelector(".show-when-active").innerText,
+            index
+        }));
+        console.log(headers);
+
+        // Select first card
+        selectCard(0);
+    });
+</script>
+
+<div class="card-selector">
+    <div class="header">
+        {#each headers as { title, img, index }}
+            <button
+                type="button"
+                style="background-image: url({img}); opacity: {selectedCard === index ? "1" : "0.75"}"
+                on:click={ () => selectCard(index) }
+            >
+                <h2>{ title }</h2>
+            </button>
+        {/each} 
+    </div>
+    <div class="content">
+        <slot/>
+    </div>
+</div>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: center;
+        gap: var(--page-spacing);
+        height: 10rem;
+        transition: scale 0.3s;
+    }
+
+    .header > button {
+        border: none;
+        background: none;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        color: white;
+        transition-duration: 0.3s;
+        transition-property: translate scale opacity;
+
+        & > h2 { margin: 0; }
+        
+        &:hover {
+            translate: 0 -10px;
+            scale: 1.05;
+        }
+    }
+
+    .content {
+        & > div {
+            transition: opacity 0.3s;
+        }
+    }
+</style>
--- src/routes/whats-on/+page.svelte
+++ src/routes/whats-on/+page.svelte
@@ -1,15 +1,78 @@
 <script>
     import { _ } from "svelte-i18n";
+    import CardSelection from "$lib/components/CardSelection.svelte";
+
+    // console.log($_("whats_on"));
+    // convert string to template string, updates when language changes
+    // thought `` would make <strong></strong> work, it doesn't. it could work with docu.element.innerHTML()? maybe will try page is finished
+    let storage = [];
+    let context;
+    $: {
+        storage = [];
+        context = $_("whats_on.random_international.description");
+        context.forEach((element) => {
+            storage.push(eval("{`" + element + "`}"));
+        });
+    }
+    // String.prototype.interpolate = function (params) {
+    //     const names = Object.keys(params);
+    //     const vals = Object.values(params);
+    //     return new Function(...names, `return \`${this}\`;`)(...vals);
+    // };
+    // let template = "Example text: aaaaaaaaaaaa".interpolate({});
+    // console.log(template);
+    // console.log(context);
 </script>
 
 <section id="whats-on-page-content">
-    <h1>{ $_("header.whats_on") }</h1>
+    <h1>{$_("header.whats_on")}</h1>
+    <article class="content">
+        <h2 class="paragraph">{$_("whats_on.random_international.name")}</h2>
+        {#each storage as item}
+            <p class="paragraph">{item}</p>
+        {/each}
+    </article>
+    <CardSelection>
+        <div>
+            <h2 class="card-title">Tentoonstellaaaaing #1</h2>
+            <img
+                class="card-img"
+                alt="card"
+                src="https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png"
+            />
+        </div>
+        <div>
+            <h2 class="card-title">Tentoonstellaaaing #2</h2>
+            <p class="show-when-active">nealsdjfuoargg???</p>
+            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+            <img
+                class="card-img"
+                alt="card"
+                src="https://cdn11.bigcommerce.com/s-3uewkq06zr/images/stencil/1280x1280/products/230/406/blue_b__05623.1492487362.png"
+            />
+        </div>
+    </CardSelection>
 </section>
 
-<style>    
+<style>
     #whats-on-page-content {
         padding: var(--page-spacing);
         box-sizing: border-box;
+    }
+
+    .content {
+        display: flex;
+        flex-flow: column wrap;
+        align-content: center;
+        justify-content: flex-start;
+        gap: 1.5rem /*var(--page-spacing) */;
+    }
+
+    .paragraph {
+        font-size: 1.5rem;
+        max-width: 800px;
+        width: 60%;
+        flex: 1 0 60%;
     }
 
     h1 {
