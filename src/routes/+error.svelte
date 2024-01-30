9913c5f1e67f3997f3aa6193460f3c38ae547775
Mick Negenman <36791@ma-web.nl>
2024-01-30 10:41:36 +0100

    Added hero text

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,29 +2,43 @@
     import { page } from '$app/stores';
 </script>
 
-<div>
-    <section>
-        <img src="img/logo-and-title.png" alt="logo"/>
-        <h1>{$page.status}: {$page.error.message}</h1>
-    </section>
-</div>
+<section>
+    <div id="dino-game">
+        <div id="dino"></div>
+    </div>
+    <h1>{$page.status}: {$page.error.message}</h1>
+</section>
 
 <style>
-    div {
-        display: flex;
-        justify-content: center;
-        margin: 10% 0;
-    }
-
     section {
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap: 50px;
-        width: 30%;
+        width: 50%;
+        font-family: "Segoe UI";
+        position: relative;
+        left: 50%;
+        translate: -50%;
+        padding: var(--page-spacing);
     }
 
-    img {
+    h1 {
+        margin: 0;
+        margin-top: var(--page-spacing);
+    }
+
+    #dino-game {
+        background-color: gray;
         width: 100%;
+        height: 13rem;
+        border-bottom: white solid thin;
+    }
+
+    #dino {
+        width: 2rem;
+        height: 2rem;
+        background-color: white;
+        position: relative;
+        top: calc(100% - 2rem);
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 <section id="main-page-content">
     <Article
         title="NXT Museum"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        content={ $_("home.hero_text") }
         direction="left"
         style="max-width: 60%;"
         titleStyle="font-size: 6rem;"/>
